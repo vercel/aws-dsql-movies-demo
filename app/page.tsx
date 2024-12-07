18a773937d027c98a61d5866b0276a7219d0b052
Lee Robinson <lrobinson2011@gmail.com>
2024-12-07 16:37:48 -0600

    DynamicIO

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,26 @@ import { getMovies } from '@/lib/db/queries';
 import { cookies } from 'next/headers';
 import { Suspense } from 'react';
 
-export default async function HomePage({
-  searchParams,
-}: {
+type Props = {
   searchParams: Promise<{ filter: string | undefined }>;
-}) {
+};
+
+export default async function HomePage({ searchParams }: Props) {
+  return (
+    <div className="container mx-auto p-4 max-w-3xl">
+      <Explanation />
+      <Suspense fallback={<Loading />}>
+        <Movies searchParams={searchParams} />
+      </Suspense>
+    </div>
+  );
+}
+
+async function Movies({ searchParams }: Props) {
   const { filter } = await searchParams;
   const cookieStore = await cookies();
   const sessionId = cookieStore.get('sessionId')?.value;
   const movies = await getMovies(sessionId);
 
-  return (
-    <div className="container mx-auto p-4 max-w-3xl">
-      <Explanation />
-      <Suspense fallback={<Loading />}>
-        <MovieVoting movies={movies} highlight={filter || ''} />
-      </Suspense>
-    </div>
-  );
+  return <MovieVoting movies={movies} highlight={filter || ''} />;
 }
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   experimental: {
-    // dynamicIO: true,
+    dynamicIO: true,
     ppr: true,
     inlineCss: true,
   },
