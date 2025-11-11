84e7685f079be2a8545fd0fd3e62b28dfd2e1591
Zak van der Merwe <zakv@amazon.com>
2025-11-11 11:08:41 -0800

    Improve DSQL interactions
    - Use `@aws/aurora-dsql-node-postgres-connector` to simplify connection
    pool creation.
    
    - Speed up initial data load by inserting in batches which results in
    fewer roundtrips. This required node-pg-format.
    
    - Simplified the migrate script a bit by removing the use of `--> statement-breakpoint`
    
    - Added Prettier config
    
    - Some dependency upgrades to remove some of the warnings

--- lib/db/db.ts
+++ lib/db/db.ts
@@ -1,71 +1,31 @@
-import { awsCredentialsProvider } from '@vercel/functions/oidc';
-import { DsqlSigner } from '@aws-sdk/dsql-signer';
+import { AuroraDSQLPool } from '@aws/aurora-dsql-node-postgres-connector';
 import { Pool } from 'pg';
 import { config } from 'dotenv';
 
 config();
 
 let pool: Pool | null = null;
-let cachedToken: { token: string; expiresAt: Date } | null = null;
 
-export async function getToken() {
-  const now = new Date();
-  if (cachedToken && cachedToken.expiresAt > now) {
-    return cachedToken.token;
-  }
-
-  const region = process.env.AWS_REGION || 'us-east-1';
-  // DSQL token max validity is 604,800 seconds (1 week). We are using 1 hour in this case.
-  const expiresInSeconds = 3600;
-  const signer = new DsqlSigner({
-    hostname: process.env.DB_CLUSTER_ENDPOINT!,
-    region: region,
-    expiresIn: expiresInSeconds,
-    credentials: awsCredentialsProvider({
-      roleArn: process.env.AWS_ROLE_ARN!,
-    }),
-  });
-
-  const token = await signer.getDbConnectAdminAuthToken();
-
-  const expiresAt = new Date(now.getTime() + (expiresInSeconds - 60) * 1000);
-  cachedToken = { token, expiresAt };
-
-  return token;
-}
-
-export async function getConnection() {
-  const now = new Date();
-
-  if (pool && cachedToken && cachedToken.expiresAt > now) {
+export async function getPool() {
+  if (pool) {
     return pool;
   }
 
   try {
-    if (pool) {
-      await pool.end();
-      pool = null;
-    }
-
-    const token = await getToken();
-
-    pool = new Pool({
+    pool = new AuroraDSQLPool({
       host: process.env.DB_CLUSTER_ENDPOINT!,
+      region: process.env.AWS_REGION || 'us-east-1',
       user: 'admin',
-      password: token,
-      database: 'postgres',
-      port: 5432,
-      ssl: true,
       max: 20,
     });
     return pool;
   } catch (error) {
-    console.error('Failed to create database connection:', error);
+    console.error('Failed to create database connection pool:', error);
     throw error;
   }
 }
 
-export async function closeConnection() {
+export async function closePool() {
   if (pool) {
     await pool.end();
     pool = null;
