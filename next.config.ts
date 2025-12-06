afdbec3376f18ab94d44620842e5824b1e5a27ad
dvoytenko <dima.voytenko@vercel.com>
2025-12-05 16:27:43 -0800

    remove experiments

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    dynamicIO: true,
-    inlineCss: true,
-  },
+  experimental: {},
 };
 
 export default nextConfig;
