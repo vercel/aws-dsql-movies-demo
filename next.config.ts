0cce242e2fa269b45aa7dfe2b4beea20de6fced5
dvoytenko <dima.voytenko@vercel.com>
2025-12-05 16:25:30 -0800

    remove ppr experiment

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
     dynamicIO: true,
-    ppr: true,
     inlineCss: true,
   },
 };
